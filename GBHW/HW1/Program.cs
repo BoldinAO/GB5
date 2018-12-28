e9d58ac7aff008884454b0fc9940e1c06e985350
UGSK\BoldinAO <boldinao@ugsk.ru>
2018-12-28 16:03:55 +0500

    Доделана 4 задание и начато 5 задание

--- GBHW/HW1/Program.cs
+++ GBHW/HW1/Program.cs
@@ -1,6 +1,27 @@
-﻿using System;
+﻿//Болдин
+/*
+4. Задача ЕГЭ.
+*На вход программе подаются сведения о сдаче экзаменов учениками 9-х классов некоторой средней
+школы. В первой строке сообщается количество учеников N, которое не меньше 10, но не
+превосходит 100, каждая из следующих N строк имеет следующий формат:
+<Фамилия> <Имя> <оценки>,
+где <Фамилия> — строка, состоящая не более чем из 20 символов, <Имя> — строка, состоящая не
+более чем из 15 символов, <оценки> — через пробел три целых числа, соответствующие оценкам по
+пятибалльной системе. <Фамилия> и <Имя>, а также <Имя> и <оценки> разделены одним пробелом.
+Пример входной строки:
+Иванов Петр 4 5 3
+Требуется написать как можно более эффективную программу, которая будет выводить на экран
+фамилии и имена трёх худших по среднему баллу учеников. Если среди остальных есть ученики,
+набравшие тот же средний балл, что и один из трёх худших, следует вывести и их фамилии и имена.
+Достаточно решить 2 задачи. Старайтесь разбивать программы на подпрограммы. Переписывайте в
+начало программы условие и свою фамилию. Все программы сделать в одном решении. Для решения
+задач используйте неизменяемые строки (string)
+*/
+
+using System;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HW1
 {
@@ -11,7 +32,7 @@ namespace HW1
             Students st = new Students();
             Console.WriteLine("Введите кол-во учеников");
             Students.SetQuantity(Console.ReadLine());
-            st.SetFIR();
+            st.SetSNR();
             st.GetN();
             Console.ReadKey();
         }
@@ -29,57 +50,80 @@ namespace HW1
                     get { return Marks[i]; }
                     set { Marks[i] = value; }
                 }
+                public double NodMark { get; set; }
             }
 
             static int quantity { get; set; }
-            Regex FIR = new Regex(@"^[А-Я]{1}[а-я]{0,19}\s{1}[А-Я]{1}[а-я]{0,14}\s{1}([2-5]\s{1}){2}[2-5]{1}$");
+            Regex SNR = new Regex(@"^[А-Я]{1}[а-я]{0,19}\s{1}[А-Я]{1}[а-я]{0,14}\s{1}([2-5]\s{1}){2}[2-5]{1}$");
 
             public static void SetQuantity(string q)
             {
-                int qq = int.Parse(q);
-                quantity = qq > 0 ? (qq < 101 ? qq : 0) : 0;
+                try
+                {
+                    int qq = int.Parse(q);
+                    quantity = qq > 9 ? (qq < 101 ? qq : 0) : 0;
+                    if (quantity == 0)
+                        Console.WriteLine("Необходимо ввести кол-во учеников от 10 до 100!");
+                }
+                catch
+                {
+                    Console.WriteLine("Необходимо ввести кол-во учеников от 10 до 100!");
+                }
             }
 
-            public void SetFIR()
+            public void SetSNR()
             {
+                string _SNR;
                 for (int i = 0; i < quantity; i++)
                 {
-                    string _FIR = Console.ReadLine();
-                    if (FIR.IsMatch(_FIR))
+                    do
                     {
-                        Student.Add(new StudentInfo { Surname = _FIR.Split()[0], Name = _FIR.Split()[1], [0] = int.Parse(_FIR.Split()[2]), [1] = int.Parse(_FIR.Split()[3]), [2] = int.Parse(_FIR.Split()[4]) });
-                    }
+                        Console.WriteLine("Введите фамилию, имя и три оценки {0} ученика:", i + 1);
+                        _SNR = Console.ReadLine();
+                        if (SNR.IsMatch(_SNR))
+                        {
+                            Student.Add(new StudentInfo
+                            {
+                                Surname = _SNR.Split()[0],
+                                Name = _SNR.Split()[1],
+                                [0] = int.Parse(_SNR.Split()[2]),
+                                [1] = int.Parse(_SNR.Split()[3]),
+                                [2] = int.Parse(_SNR.Split()[4]),
+                            });
+                            Student[i].NodMark = (Student[i][0] + Student[i][1] + Student[i][2]) / 3.0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неверный ввод");
+                        }
+                    } while (!SNR.IsMatch(_SNR));
                 }
-                Console.WriteLine($"{Student[0].Name} {Student[0].Surname} {Student[0][2]}");
             }
 
             public void GetN()
             {
-                int marks;
-                double[] nodMark = new double[3];
-                bool flag = false;
-                for(int i = 0; i < 3; i++)
+                if(Student.Count > 0)
                 {
-                    marks = 0;
-                    for (int x = 0; x < 3; x++)
-                    {
-                        marks += Student[i][x];
-                    }
-                    nodMark[i] = marks / 3.0;
-                    Console.WriteLine(nodMark[i]);
-                }
+                    Student.Sort(delegate (StudentInfo student, StudentInfo student2)
+                    { return student.NodMark.CompareTo(student2.NodMark); });
 
-                for(int i = 0; i < 3; i++)
-                {
-                    for (int x = 0; x < 3; x++)
+                    var uniqueMarks =
+                    from StudentInfo n in Student.Distinct()
+                    group n.NodMark by n.NodMark into nGroup
+                    select nGroup.Key;
+
+                    Console.WriteLine("Список учеников с плохой средней оценкой:");
+                    foreach (double mark in uniqueMarks.Take(3))
                     {
-                        if (nodMark[i] < nodMark[x])
-                            flag = true;
-                        else
-                            flag = false;
+                        foreach (StudentInfo st in Student)
+                        {
+                            if (st.NodMark == mark)
+                                Console.WriteLine($"{st.Surname} {st.Name} {st.NodMark}");
+                        }
                     }
-                    if (flag)
-                        Console.WriteLine($"{Student[i].Surname} {Student[i].Name}");
+                } else
+                {
+                    Console.WriteLine("Список учеников отсутствует!");
                 }
             }
         }
---
+++ GBHW/HW2/Program.cs
@@ -0,0 +1,91 @@
+﻿//Болдин
+/*
+Написать игру «Верю. Не верю». В файле хранятся вопрос и ответ, правда это или нет.
+Например: «Шариковую ручку изобрели в древнем Египте», «Да».
+Компьютер загружает эти данные, случайным образом выбирает 5 вопросов и задаёт их игроку.
+Игрок отвечает Да или Нет на каждый вопрос и набирает баллы за каждый правильный ответ.
+Список вопросов ищите во вложении или воспользуйтесь интернетом.
+*/
+
+#region пока дичь
+using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game g = new Game();
+            g.Start();
+            Console.ReadKey();
+        }
+
+        class Questions
+        {
+            string[] sQuestions = File.ReadAllLines("q.txt", Encoding.GetEncoding(1251));
+            string[] sAnswers = File.ReadAllLines("a.txt", Encoding.GetEncoding(1251));
+            private Dictionary<int, string> Question, Answer;
+
+            public string this[string AQ, int index]
+            {
+                get {
+                    if (AQ == "1")
+                        return sAnswers[index];
+                    return sQuestions[index];
+                }
+                set { sAnswers[index] = value;  }
+            }
+
+
+
+            public Questions()
+            {
+                Question = new Dictionary<int, string>();
+                int i = 0;
+                foreach (string m in sQuestions)
+                {
+                    Question.Add(i++, m);
+                }
+
+                i = 0;
+                Answer = new Dictionary<int, string>();
+                foreach (string m in sAnswers)
+                {
+                    Answer.Add(i++, m);
+                }
+            }
+        }
+
+        class Game
+        {
+            Questions q = new Questions();
+            public void Start()
+            {
+                int count = 0;
+                while (true)
+                {
+                    Random randomQuestion = new Random();
+                    int rand = randomQuestion.Next(Question.Count);
+                    Console.WriteLine(Question[rand]);
+                    Console.WriteLine("Ваш ответ: ");
+                    if (Check(rand, Console.ReadLine()))
+                        count++;
+                    Console.WriteLine("{0} правильных ответов", count);
+                }
+            }
+            bool Check(int s, string g)
+            {
+                if (g == Answer[s])
+                    return true;
+                else
+                    return false;
+            }
+        }
+    }
+
+}
+#endregion
